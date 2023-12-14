b9e9daf1b9d82826d76ed97f4623406e908e15d9
krkim <kgs08101220@naver.com>
2023-12-14 10:36:01 +0900

    make basic

---
+++ basic/src/main/java/com/example/basic/database/dao/impl/WebDaoImpl.java
@@ -0,0 +1,8 @@
+package com.example.basic.database.dao.impl;
+
+import com.example.basic.database.dao.WebDao;
+import com.example.basic.model.repository.WebRepository;
+
+public class WebDaoImpl implements WebDao {
+    private WebRepository webRepository;
+}
