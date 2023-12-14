8ea1482937f7d144b3ab1bff3e22dcb287b3db03
krkim <kgs08101220@naver.com>
2023-12-14 16:21:26 +0900

    index css

---
+++ freezing/src/test/java/com/example/freezing/FreezingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freezing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreezingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
